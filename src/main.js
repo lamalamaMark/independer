2afb7f90c275682709492337cbfb8db1ffec43f6
Mark de Vries <mark@lamalama.nl>
2025-02-18 11:31:26 +0100

    wip

--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
 import "./style.css";
+
+document.addEventListener("DOMContentLoaded", () => {
+  // Get the elements
+  const firstPage = document.querySelector("main#questionnaire");
+  const secondPage = document.querySelector("main#result");
+  const compareButton = document.querySelector("button#compare-button");
+
+  // Add click event listener to the compare button
+  compareButton.addEventListener("click", () => {
+    firstPage.classList.add("hidden");
+    secondPage.classList.remove("hidden");
+  });
+});
