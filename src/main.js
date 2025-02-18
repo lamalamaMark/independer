1fc65d1617aa0f61022b075e4f6bdf463917e5a6
Mark de Vries <mark@lamalama.nl>
2025-02-18 19:53:56 +0100

    wip

--- src/main.js
+++ src/main.js
@@ -5,10 +5,18 @@ document.addEventListener("DOMContentLoaded", () => {
   const firstPage = document.querySelector("main#questionnaire");
   const secondPage = document.querySelector("main#result");
   const compareButton = document.querySelector("button#compare-button");
+  const homeButtons = document.querySelectorAll(".js-to-home");
 
   // Add click event listener to the compare button
   compareButton.addEventListener("click", () => {
     firstPage.classList.add("hidden");
     secondPage.classList.remove("hidden");
+  });
+
+  homeButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      firstPage.classList.remove("hidden");
+      secondPage.classList.add("hidden");
+    });
   });
 });
